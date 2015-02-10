85a328d4aef2bb3d82bedc5235905d2f2f248967
Manuel Freiberger <manuel.freiberger@gmx.at>
2015-02-10 22:10:25 +0100

    Stricter tests for the asynchronous state machine.

--- test/tst_statemachine.cpp
+++ test/tst_statemachine.cpp
@@ -4,11 +4,23 @@
 
 using namespace fsm11;
 
+
+namespace sync
+{
 using StateMachine_t = fsm11::StateMachine<>;
 using State_t = StateMachine_t::state_type;
+} // namespace sync
+
+namespace async
+{
+using StateMachine_t = StateMachine<AsynchronousEventDispatching>;
+using State_t = StateMachine_t::state_type;
+} // namespace async
+
 
 TEST_CASE("construct a statemachine", "[statemachine]")
 {
+    using namespace sync;
     StateMachine_t sm;
     REQUIRE(!sm.running());
     REQUIRE(sm.numConfigurationChanges() == 0);
@@ -16,6 +28,7 @@ TEST_CASE("construct a statemachine", "[statemachine]")
 
 TEST_CASE("start an empty synchronous statemachine", "[statemachine]")
 {
+    using namespace sync;
     StateMachine_t sm;
     REQUIRE(!sm.running());
     REQUIRE(!sm.isActive());
@@ -34,15 +47,18 @@ TEST_CASE("start an empty synchronous statemachine", "[statemachine]")
 
 TEST_CASE("start an empty asynchronous statemachine", "[statemachine]")
 {
-    using StateMachine_t = StateMachine<AsynchronousEventDispatching>;
-
+    using namespace async;
     StateMachine_t sm;
     REQUIRE(!sm.running());
     for (int cnt = 0; cnt < 2; ++cnt)
     {
         sm.start();
         REQUIRE(sm.running());
+        REQUIRE(sm.isActive());
+        REQUIRE(sm.numConfigurationChanges() == 2 * cnt + 1);
         sm.stop();
         REQUIRE(!sm.running());
+        REQUIRE(!sm.isActive());
+        REQUIRE(sm.numConfigurationChanges() == 2 * cnt + 2);
     }
 }
