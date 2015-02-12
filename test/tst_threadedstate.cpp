4497bdf6c6fe9950519909b877e82f2950d43889
Manuel Freiberger <manuel.freiberger@gmx.at>
2015-02-12 23:52:04 +0100

    ThreadedState got an exit request notification.

--- test/tst_threadedstate.cpp
+++ test/tst_threadedstate.cpp
@@ -3,6 +3,7 @@
 #include "../threadedstate.hpp"
 #include "../statemachine.hpp"
 
+#include <condition_variable>
 #include <mutex>
 #include <thread>
 
@@ -11,6 +12,9 @@ using State_t = fsm11::ThreadedState<StateMachine_t>;
 
 std::mutex g_mutex;
 std::thread::id g_id;
+std::condition_variable g_cv;
+bool g_notify;
+int g_invokeState;
 
 class TestState : public State_t
 {
@@ -21,6 +25,34 @@ public:
     {
         std::lock_guard<std::mutex> lock(g_mutex);
         g_id = std::this_thread::get_id();
+    }
+};
+
+template <unsigned TFn>
+class WaitingState : public State_t
+{
+public:
+    using State_t::State_t;
+
+    virtual void invoke() override
+    {
+        g_mutex.lock();
+        g_notify = true;
+        g_invokeState = 1;
+        g_mutex.unlock();
+        g_cv.notify_all();
+
+        switch (TFn)
+        {
+        case 0: waitForExitRequest(); break;
+        case 1: waitForExitRequestFor(std::chrono::milliseconds(500)); break;
+        }
+
+        g_mutex.lock();
+        g_notify = true;
+        g_invokeState = 2;
+        g_mutex.unlock();
+        g_cv.notify_all();
     }
 };
 
@@ -43,4 +75,102 @@ TEST_CASE("start invoke action in threaded state", "[threadedstate]")
     sm.start();
     sm.stop();
     REQUIRE(g_id != std::this_thread::get_id());
+}
+
+TEST_CASE("waitForExitRequest blocks an invoked action", "[threadedstate]")
+{
+    StateMachine_t sm;
+    WaitingState<0> s1("s1", &sm);
+
+    g_notify = false;
+    g_invokeState = 0;
+
+    sm.start();
+
+    std::unique_lock<std::mutex> lock(g_mutex);
+    g_cv.wait(lock, [&] { return g_notify; });
+    REQUIRE(g_invokeState == 1);
+    lock.unlock();
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    // The invoked action must still be blocked.
+    lock.lock();
+    g_cv.wait(lock, [&] { return g_notify; });
+    REQUIRE(g_invokeState == 1);
+    g_notify = false;
+    lock.unlock();
+
+    sm.stop();
+
+    lock.lock();
+    g_cv.wait(lock, [&] { return g_notify; });
+    REQUIRE(g_invokeState == 2);
+    lock.unlock();
+}
+
+TEST_CASE("waitForExitRequestFor blocks an invoked action", "[threadedstate]")
+{
+    StateMachine_t sm;
+    WaitingState<1> s1("s1", &sm);
+
+    g_notify = false;
+    g_invokeState = 0;
+
+    sm.start();
+
+    std::unique_lock<std::mutex> lock(g_mutex);
+    g_cv.wait(lock, [&] { return g_notify; });
+    REQUIRE(g_invokeState == 1);
+    g_notify = false;
+    lock.unlock();
+
+    SECTION("timeout")
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        lock.lock();
+        g_cv.wait(lock, [&] { return g_notify; });
+        REQUIRE(g_invokeState == 2);
+        lock.unlock();
+
+        sm.stop();
+    }
+
+    SECTION("no timeout")
+    {
+        sm.stop();
+
+        lock.lock();
+        g_cv.wait(lock, [&] { return g_notify; });
+        REQUIRE(g_invokeState == 2);
+        lock.unlock();
+    }
+}
+
+TEST_CASE("invoked action is left when state machine is destructed", "[threadedstate]")
+{
+    g_notify = false;
+    g_invokeState = 0;
+    std::unique_lock<std::mutex> lock(g_mutex, std::defer_lock);
+    std::unique_ptr<WaitingState<0>> s1;
+
+    {
+        StateMachine_t sm;
+        // The state must be destructed after sm's destructor has been called.
+        s1.reset(new WaitingState<0>("s1", &sm));
+
+        sm.start();
+
+        lock.lock();
+        g_cv.wait(lock, [&] { return g_notify; });
+        REQUIRE(g_invokeState == 1);
+        g_notify = false;
+        lock.unlock();
+    }
+
+    lock.lock();
+    g_cv.wait(lock, [&] { return g_notify; });
+    REQUIRE(g_invokeState == 2);
+    lock.unlock();
 }
--- threadedstate.hpp
+++ threadedstate.hpp
@@ -1,14 +1,23 @@
 #ifndef FSM11_THREADEDSTATE_HPP
 #define FSM11_THREADEDSTATE_HPP
 
+#include "statemachine_fwd.hpp"
 #include "state.hpp"
 
 #ifdef FSM11_USE_WEOS
+#include <weos/chrono.hpp>
+#include <weos/condition_variable.hpp>
 #include <weos/exception.hpp>
+#include <weos/mutex.hpp>
 #include <weos/thread.hpp>
+#include <weos/type_traits.hpp>
 #else
+#include <chrono>
+#include <condition_variable>
 #include <exception>
+#include <mutex>
 #include <thread>
+#include <type_traits>
 #endif // FSM11_USE_WEOS
 
 namespace fsm11
@@ -25,15 +34,48 @@ public:
 
     //! \brief Creates a state with a threaded invoke action.
     explicit ThreadedState(const char* name,
-#ifdef FSM11_USE_WEOS
-                           weos::thread::attributes attrs,
-#endif // FSM11_USE_WEOS
                            base_type* parent = 0)
         : base_type(name, parent)
-#ifdef FSM11_USE_WEOS
-        , m_invokeThreadAttributes(attrs)
-#endif // FSM11_USE_WEOS
     {
+    }
+
+#ifdef FSM11_USE_WEOS
+    //! \brief Creates a state with a threaded invoke action.
+    explicit ThreadedState(const char* name,
+                           weos::thread::attributes attrs,
+                           base_type* parent = 0)
+        : base_type(name, parent),
+          m_invokeThreadAttributes(attrs)
+    {
+    }
+#endif // FSM11_USE_WEOS
+
+    virtual ~ThreadedState()
+    {
+        // If the invoked thread is joinable, exitInvoke() has not been
+        // called. This is the case if the state is destructed before the
+        // state machine is stopped.
+        assert(!m_invokeThread.joinable());
+    }
+
+    bool exitRequested() const
+    {
+        FSM11STD::lock_guard<FSM11STD::mutex> lock(m_mutex);
+        return m_exitRequested;
+    }
+
+    void waitForExitRequest()
+    {
+        FSM11STD::unique_lock<FSM11STD::mutex> lock(m_mutex);
+        m_cv.wait(lock, [&] { return m_exitRequested; });
+    }
+
+    template <typename TRep, typename TPeriod>
+    bool waitForExitRequestFor(
+            const FSM11STD::chrono::duration<TRep, TPeriod>& timeout)
+    {
+        FSM11STD::unique_lock<FSM11STD::mutex> lock(m_mutex);
+        return m_cv.wait_for(lock, timeout, [&] { return m_exitRequested; });
     }
 
     //! \brief The actual invoke action.
@@ -42,8 +84,13 @@ public:
     //! override this function.
     virtual void invoke() = 0;
 
+    //! Enteres the invoked thread.
     virtual void enterInvoke() override
     {
+        m_mutex.lock();
+        m_exitRequested = false;
+        m_mutex.unlock();
+
         m_exceptionPointer = nullptr;
         m_invokeThread = FSM11STD::thread(
 #ifdef FSM11_USE_WEOS
@@ -58,6 +105,12 @@ public:
     virtual FSM11STD::exception_ptr exitInvoke() override
     {
         assert(m_invokeThread.joinable());
+
+        m_mutex.lock();
+        m_exitRequested = true;
+        m_mutex.unlock();
+        m_cv.notify_one();
+
         m_invokeThread.join();
         return m_exceptionPointer;
     }
@@ -69,6 +122,10 @@ private:
 
     FSM11STD::thread m_invokeThread;
     FSM11STD::exception_ptr m_exceptionPointer;
+
+    mutable FSM11STD::mutex m_mutex;
+    FSM11STD::condition_variable m_cv;
+    bool m_exitRequested;
 
     void invokeWrapper()
     {
