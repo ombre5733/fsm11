827f5209cd4bf5fba3f4a6590ba43e3f046ad299
Manuel Freiberger <manuel.freiberger@gmx.at>
2015-04-04 23:18:04 +0200

    Added a test to make sure that strings are suitable as events.

--- test/tst_event.cpp
+++ test/tst_event.cpp
@@ -27,7 +27,14 @@
 #include "../src/statemachine.hpp"
 #include "testutils.hpp"
 
+#include <cstring>
+#include <map>
+#include <memory>
+#include <string>
+#include <tuple>
+
 using namespace fsm11;
+
 
 enum Event1
 {
@@ -195,3 +202,180 @@ SCENARIO("integral types can be used as events", "[event]")
         }
     }
 }
+
+SCENARIO("strings are suitable as events", "[events]")
+{
+    GIVEN ("an FSM with const char* as event type")
+    {
+        using StateMachine_t = StateMachine<EventType<const char*>,
+                                            EventListType<std::deque<const char*>>>;
+        using State_t = State<StateMachine_t>;
+
+        StateMachine_t sm;
+        State_t a("a", &sm);
+        State_t b("b", &sm);
+        State_t c("c", &sm);
+
+        sm += a + event("go to B") > b;
+        sm += a + event("go to C") > c;
+
+        sm.start();
+
+        WHEN ("the event 'go to B' is added")
+        {
+            sm.addEvent("go to B");
+            THEN ("the FSM transitions to state B")
+            {
+                REQUIRE(isActive(sm, {&sm, &b}));
+            }
+
+            WHEN ("the event 'go to A' is added")
+            {
+                sm.addEvent("go to A");
+                THEN ("nothing happens")
+                {
+                    REQUIRE(isActive(sm, {&sm, &b}));
+                }
+            }
+        }
+
+        WHEN ("the event 'go to B' with a distinct address is added")
+        {
+            sm.addEvent(std::string("go to B").c_str());
+            THEN ("nothing happens")
+            {
+                REQUIRE(isActive(sm, {&sm, &a}));
+            }
+        }
+
+        WHEN ("the event 'go to C' is added")
+        {
+            sm.addEvent("go to C");
+            THEN ("the FSM transitions to state C")
+            {
+                REQUIRE(isActive(sm, {&sm, &c}));
+            }
+
+            WHEN ("the event 'go to A' is added")
+            {
+                sm.addEvent("go to A");
+                THEN ("nothing happens")
+                {
+                    REQUIRE(isActive(sm, {&sm, &c}));
+                }
+            }
+        }
+
+        WHEN ("the event 'go to X' is added")
+        {
+            sm.addEvent("go to X");
+            THEN ("nothing happens")
+            {
+                REQUIRE(isActive(sm, {&sm, &a}));
+            }
+        }
+    }
+
+    GIVEN ("an FSM with std::string as event type")
+    {
+        using StateMachine_t = StateMachine<EventType<std::string>,
+                                            EventListType<std::deque<std::string>>>;
+        using State_t = State<StateMachine_t>;
+
+        StateMachine_t sm;
+        State_t a("a", &sm);
+        State_t b("b", &sm);
+        State_t c("c", &sm);
+
+        sm += a + event("go to B") > b;
+        sm += a + event("go to C") > c;
+
+        sm.start();
+
+        WHEN ("the event 'go to B' is added")
+        {
+            sm.addEvent("go to B");
+            THEN ("the FSM transitions to state B")
+            {
+                REQUIRE(isActive(sm, {&sm, &b}));
+            }
+
+            WHEN ("the event 'go to A' is added")
+            {
+                sm.addEvent("go to A");
+                THEN ("nothing happens")
+                {
+                    REQUIRE(isActive(sm, {&sm, &b}));
+                }
+            }
+        }
+
+        WHEN ("the event 'go to C' is added")
+        {
+            sm.addEvent("go to C");
+            THEN ("the FSM transitions to state C")
+            {
+                REQUIRE(isActive(sm, {&sm, &c}));
+            }
+
+            WHEN ("the event 'go to A' is added")
+            {
+                sm.addEvent("go to A");
+                THEN ("nothing happens")
+                {
+                    REQUIRE(isActive(sm, {&sm, &c}));
+                }
+            }
+        }
+
+        WHEN ("the event 'go to X' is added")
+        {
+            sm.addEvent("go to X");
+            THEN ("nothing happens")
+            {
+                REQUIRE(isActive(sm, {&sm, &a}));
+            }
+        }
+    }
+}
+
+#if 0
+struct TrackingEvent
+{
+    TrackingEvent() = default;
+
+    TrackingEvent(int value)
+        : m_value(new int(value))
+    {
+        ++std::get<0>(m_valueToCounter[value]);
+    }
+
+    TrackingEvent(const TrackingEvent&) = delete;
+
+    TrackingEvent(TrackingEvent&& e)
+        : m_value(std::move(e.m_value))
+    {
+        if (m_value)
+            ++std::get<1>(m_valueToCounter[*m_value]);
+    }
+
+    ~TrackingEvent()
+    {
+        if (m_value)
+        {
+            ++std::get<2>(m_valueToCounter[*m_value]);
+        }
+    }
+
+    std::unique_ptr<int> m_value;
+
+    typedef std::tuple<int, int, int> counters;
+    static std::map<int, counters> m_valueToCounter;
+};
+
+std::map<int, TrackingEvent::counters> TrackingEvent::m_valueToCounter;
+
+SCENARIO("movable types can be used as event", "[event]")
+{
+}
+#endif
